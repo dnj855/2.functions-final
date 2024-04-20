a412b19f53cf56f1a3c960acba4fe92b9a1a64d5
Cédric Lang-Roth <contact@clangroth.fr>
2024-04-20 21:23:56 +0200

    Add main function.

--- index.js
+++ index.js
@@ -26,7 +26,23 @@ const characterInitialisation = (
   return characters;
 };
 
-// const length = prompt("🔢 Combien de caractères ? (8-36) ");
-// const specials = prompt("🔣 Caractères spéciaux ? (y/n) ");
-// const numbers = prompt("🔢 Chiffres ? (y/n) ");
-// const capitals = prompt("⬆️ Majuscules ? (y/n) ");
+const passwordGenerator = () => {
+  //TODO: Add a check for every input and a recursivity if the input is not correct
+  const length = prompt("🔢 Combien de caractères ? (8-36) ");
+  const specials = prompt("🔣 Caractères spéciaux ? (y/n) ");
+  const numbers = prompt("🔢 Chiffres ? (y/n) ");
+  const capitals = prompt("⬆️ Majuscules ? (y/n) ");
+  const characters = characterInitialisation(
+    specials === "y",
+    numbers === "y",
+    capitals === "y"
+  );
+  let password = "";
+  for (let i = 0; i < Number(length); i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+  console.log("Votre mot de passe généré est :", password);
+};
+
+passwordGenerator();
